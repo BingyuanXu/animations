7e28d4b8fb558eca02a8891faa085f06b222a016
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-23 07:53:59 -0500

    explicated animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,58 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var animationAmount : CGFloat = 1
-  @State private var animationAmountTwo : CGFloat = 1
+  @State private var animationAmount = 0.0
   
   var body: some View {
-    NavigationView {
-    VStack {
-      
-      Stepper("Scale amount", value: $animationAmountTwo.animation(), in: 1...10)
-        .padding()
+    Button("Tap Me") {
+      withAnimation(Animation.interpolatingSpring(stiffness: 10, damping: 2)
+        .repeatForever()
         
-      
-      Spacer()
-      
-      Button("Tap me") {
-        self.animationAmountTwo += 1
+      ) {
+        self.animationAmount += 360
       }
-      .padding(40)
-      .background(Color.red)
-      .foregroundColor(Color.white)
-      .clipShape(Circle())
-      .scaleEffect(animationAmountTwo)
       
-      Spacer()
-      
-      Button("tap me ") {
-        
-        self.animationAmount = 3
-      }
-      .padding(50)
-      .background(Color.red)
-      .foregroundColor(Color.white)
-      .clipShape(Circle())
-        //    .scaleEffect(animationAmount)
-        //    .blur(radius: animationAmount - 1)
-        .overlay(
-          Circle()
-            .stroke(Color.red)
-            .scaleEffect(animationAmount)
-            .opacity(Double(2 - animationAmount))
-            
-            .animation(Animation.easeInOut(duration: 1.5)
-              .repeatCount(1, autoreverses: false)
-              //        .repeatForever(autoreverses: false)
-              
-          )
-      )
     }
-    //      .onAppear {
-    //        self.animationAmount = 2
-    //    }
-      .navigationBarTitle("Animation")
-    }
+    .padding(50)
+    .background(Color.red)
+    .foregroundColor(.white)
+    .clipShape(Circle())
+    .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y:1, z: 1))
   }
 }
 
