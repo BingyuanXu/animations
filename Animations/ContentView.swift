2dfdb7154a81955a3fc3dc89da9b8be356187429
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-23 03:26:43 -0500

    Initial Commit

---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by bingyuan xu on 2020-05-23.
+//  Copyright © 2020 bingyuan xu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
