bf76b7367fd1b986a6ce30f842755740df058495
BingyuanXu <xubingyuan123@hotmail.com>
2020-05-23 05:20:17 -0500

    binding animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,34 +10,57 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var animationAmount : CGFloat = 1
+  @State private var animationAmountTwo : CGFloat = 1
   
   var body: some View {
-    Button("tap me ") {
+    NavigationView {
+    VStack {
       
-      self.animationAmount = 3
-    }
-    .padding(50)
-    .background(Color.red)
-    .foregroundColor(Color.white)
-    .clipShape(Circle())
-      //    .scaleEffect(animationAmount)
-      //    .blur(radius: animationAmount - 1)
-      .overlay(
-        Circle()
-          .stroke(Color.red)
-          .scaleEffect(animationAmount)
-          .opacity(Double(2 - animationAmount))
-          
-          .animation(Animation.easeInOut(duration: 1.5)
-            .repeatCount(1, autoreverses: false)
-            //        .repeatForever(autoreverses: false)
+      Stepper("Scale amount", value: $animationAmountTwo.animation(), in: 1...10)
+        .padding()
+        
+      
+      Spacer()
+      
+      Button("Tap me") {
+        self.animationAmountTwo += 1
+      }
+      .padding(40)
+      .background(Color.red)
+      .foregroundColor(Color.white)
+      .clipShape(Circle())
+      .scaleEffect(animationAmountTwo)
+      
+      Spacer()
+      
+      Button("tap me ") {
+        
+        self.animationAmount = 3
+      }
+      .padding(50)
+      .background(Color.red)
+      .foregroundColor(Color.white)
+      .clipShape(Circle())
+        //    .scaleEffect(animationAmount)
+        //    .blur(radius: animationAmount - 1)
+        .overlay(
+          Circle()
+            .stroke(Color.red)
+            .scaleEffect(animationAmount)
+            .opacity(Double(2 - animationAmount))
             
-        )
-    )
-//      .onAppear {
-//        self.animationAmount = 2
-//    }
-    
+            .animation(Animation.easeInOut(duration: 1.5)
+              .repeatCount(1, autoreverses: false)
+              //        .repeatForever(autoreverses: false)
+              
+          )
+      )
+    }
+    //      .onAppear {
+    //        self.animationAmount = 2
+    //    }
+      .navigationBarTitle("Animation")
+    }
   }
 }
 
